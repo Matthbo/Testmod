9fcfc096ba2c032b30ffb1e0880d11d02c2cc2e4
Matthijs <matthijs.booman@hotmail.com>
2014-07-12 12:01:16 +0200

    version fix
    oops

--- src/main/java/matthbo/mods/testmod/reference/Refs.java
+++ src/main/java/matthbo/mods/testmod/reference/Refs.java
@@ -4,7 +4,7 @@ public class Refs {
 	
 	public static final String MOD_ID = "Testmod";
 	public static final String MOD_NAME = "Testmod";
-	public static final String VERSION = "1.0-1.7.10";
+	public static final String VERSION = "1.7.10-1.0";
 	public static final String CLIENT_PROXY_CLASS = "matthbo.mods.testmod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "matthbo.mods.testmod.proxy.ServerProxy";
 	public static final String GUI_FACTORY_CLASS = "matthbo.mods.testmod.client.gui.GuiFactory";
