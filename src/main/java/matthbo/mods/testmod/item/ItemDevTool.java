c339cb8787ca959bdbcd57f76df23237d28ebe0e
Matthijs <matthijs.booman@hotmail.com>
2014-07-20 22:14:52 +0200

    devtool stuff

--- src/main/java/matthbo/mods/testmod/item/ItemDevTool.java
+++ src/main/java/matthbo/mods/testmod/item/ItemDevTool.java
@@ -1,10 +1,22 @@
 package matthbo.mods.testmod.item;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class ItemDevTool extends ItemTestmod {
 
 	public ItemDevTool(){
 		super();
 		this.setUnlocalizedName("devTool");
+		this.maxStackSize = 1;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public boolean isFull3D() {
+		return true;
 	}
 	
 }
